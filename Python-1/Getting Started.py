6893e1e6b9e7b1f9b1d8ab892f714b98273329dd
rishav1808 <rishavraj1808@gmail.com>
2021-10-15 22:20:41 +0530

    Pyhton-Tutorials

---
+++ Python-1/Getting Started.py
@@ -0,0 +1,15 @@
+''' There are Two types of Programming Languages
+    1.Compiled Language: The language where the source code file is converted to an executable file(.exe) which basically contains machine level code(Binary)
+                         Ex:C++
+    2.Interpreted Language:where the source code is read by an Interpreter Line-by-Line and executed on the machine
+                            Even though there is some error but still the lines above the error line will run fine.
+                            Ex:Python
+                            Python Virtual Machine acts as Interpreter and reads the source code line by line
+
+    
+ '''
+print("Hi Rishav")
+print("Welcome")
+#Now error will occur but the above line will work perfectly
+print(20/0)
+#To open Jupyter Notebook, in the desired location open the cmd and enter 'jupyter-notebook'
